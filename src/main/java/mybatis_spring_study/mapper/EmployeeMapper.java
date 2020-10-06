9360cd2d044f4643356b5afaaf57fde6eb2eb3d0
digi5633 <digi5633@gmail.com>
2020-10-06 12:10:39 +0900

    dept, emp mappers 끝!

---
+++ src/main/java/mybatis_spring_study/mapper/EmployeeMapper.java
@@ -0,0 +1,19 @@
+package mybatis_spring_study.mapper;
+
+import java.util.List;
+
+import mybatis_spring_study.dto.Employee;
+
+public interface EmployeeMapper {
+	
+	int insertEmployee(Employee employee);
+
+	int deleteEmployee(Employee employee);
+
+	int updateEmployee(Employee employee);
+
+	List<Employee> selectEmployeeByAll();
+
+	Employee selectEmployeeByNo(Employee employee);
+	
+}
